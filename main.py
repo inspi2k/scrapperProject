e659b72d75844753f4929b91d71f5a0cdfd2927a
inspi <inspi@daum.net>
2021-03-23 02:18:09 +0900

    modify config vars

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
-import os.path
+import os
 import telegram
 from time import sleep
 import datetime
+import dotenv
+
 
 # 전주시청 게시판 스크래핑
 def scrap_board(
@@ -14,32 +16,35 @@ def scrap_board(
         domain='http://www.jeonju.go.kr/planweb/board',
         mobile=''
 ):
-    global bot_channel
+    global TELEGRAM_CHANNEL
     global bot
+    global BOT_MSG_LIMIT, BOT_MSG_SLEEP, bot_msg_count
 
     # 이중반복문(while-페이지 넘어가서 계속 스크래핑 수행)을 빠져나오기 위한 체크 변수
     recent_break = False
 
     # 가장 최근 게시물 번호
-    latest_no = 0
+    latest_no = 1
 
     # 최근 게시물 번호를 file-based 에서 heroku config vars 로 변경
     config_vars_latest = 'LATEST_' + file_scrap.upper()
 
     try:
         latest_no = int(os.environ.get(config_vars_latest))
-    except (KeyError, TypeError):
+        # print(f'latest_no={latest_no}')
+    except KeyError:
         recent_break = True  # 처음 실행될 때는 1페이지만 읽어오고, 환경변수 세팅함
-        print(f'Setting config var - {config_vars_latest}')
-        os.system(f'heroku config:set {config_vars_latest}=0')
+        # print(f'Setting config var - {config_vars_latest}')
+        os.environ[config_vars_latest] = 0
+        os.system('heroku config:set ' + config_vars_latest + '=0')
+    except TypeError:
+        recent_break = True
+        # print(f'config_vars_latest={config_vars_latest}')
+        # print('os.environ[' + config_vars_latest + ']=' + os.environ[config_vars_latest])
 
     # 가장 마지막에 가져온 (저장돼 있는) 최근 게시물 번호
     recent_no = latest_no  # 최신 게시글 번호를 저장하기 위해 저장
     scrap_count = 0  # 스크래핑 해 온 게시글 수
-
-    bot_msg_limit = 20  # 20 msgs per minute to the same group
-    bot_msg_limit_sleep = 60
-    bot_msg_count = 0
 
     # HTML 스크래핑 해오기
     page_num = 1  # get parameter 'startPage'
@@ -70,11 +75,13 @@ def scrap_board(
                 latest_no = num
 
             # 최신 게시글이 있는지 확인
-            if num == recent_no:
+            if (num == recent_no) or (recent_break == True):
                 # print(f'latest={str(latest_no)}, num={str(num)}, recent={str(recent_no)}')
                 print(f'{title_scrap} / {scrap_count}개의 게시글을 가져왔습니다.')
-                # file-based 에서 환경변수 config var로 변경
-                os.environ[config_vars_latest] = latest_no
+                # file-based 에서 환경변수 config var 로 변경
+                os.environ[config_vars_latest] = str(latest_no)
+                dotenv.set_key(dotenv_file, config_vars_latest, os.environ[config_vars_latest])
+                os.system('heroku config:set ' + config_vars_latest + '=' + os.environ[config_vars_latest])
                 recent_break = True
                 break
 
@@ -88,14 +95,15 @@ def scrap_board(
             print(f'[{str(num)}]{date}/{title}({author})')
             # 시간(번호)순으로 할 때 역순으로 출력해야 하지만 주기적으로 실행하면 게시글을 많이 가져오지 않으므로 그냥 출력
 
-            bot.sendMessage(bot_channel, message, parse_mode='Markdown', disable_web_page_preview=True)
+            # print(f'BOT_MSG_LIMIT={BOT_MSG_LIMIT},BOT_MSG_SLEEP={BOT_MSG_SLEEP},bot_msg_count={bot_msg_count},scrap_count={scrap_count}')
+            if (bot_msg_count >= BOT_MSG_LIMIT) and (bot_msg_count % BOT_MSG_LIMIT) == 0:
+                print('My bot will not be able to send more than 20 messages per minute to the same group.')
+                print('Waiting a minute.')
+                sleep(BOT_MSG_SLEEP)
+
+            bot.sendMessage(TELEGRAM_CHANNEL, message, parse_mode='Markdown', disable_web_page_preview=True)
             bot_msg_count += 1
             scrap_count += 1
-
-            if (bot_msg_count % bot_msg_limit) == 0:
-                print('bot will not be able to send more than 20 messages per minute to the same group.')
-                print('waiting a minute.')
-                sleep(bot_msg_limit_sleep)
 
         # 반복문이 다 끝나더라도 최신 게시글이 안 나왔으면
         # => 페이지를 넘겨가서 나올 때까지 해야 함
@@ -104,29 +112,26 @@ def scrap_board(
         else:
             page_num += 1  # 다음페이지의 게시글을 스크래핑 해 오기 위해 페이지번호 설정
 
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
 
-    # telegram token, channel check
-    with open('token.txt', 'r') as fp:
-        try:
-            lines = fp.readlines()
-        except FileNotFoundError:
-            print('\'token.txt\' file not found.')
-            exit(1)
-        try:
-            bot_token = lines[0].strip()
-            bot_channel = lines[1].strip()
-        except IndexError:
-            print('Check file. \'token.txt\'')
-            exit(1)
+    dotenv_file = dotenv.find_dotenv()
+    dotenv.load_dotenv(dotenv_file, verbose=True)
 
-        if bot_token == '' or bot_channel == '':
-            print('Check token or channel information.')
-            exit(1)
+    TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+    TELEGRAM_CHANNEL = os.getenv('TELEGRAM_CHANNEL')
 
-    bot = telegram.Bot(bot_token)
+    bot = telegram.Bot(TELEGRAM_TOKEN)
 
+    BOT_MSG_LIMIT = 20  # 20 msgs per minute to the same group
+    BOT_MSG_SLEEP = 60
+    bot_msg_count = 0
+    '''
+    keys = os.environ.keys()
+    for item in keys:
+        print('%s=%s' %(item, os.environ[item]))
+    '''
     url_jeonju = [
         '/list.9is?boardUid=9be517a74f8dee91014f90e8502d0602&page=',
         '전주시 새소식',
@@ -150,5 +155,12 @@ if __name__ == '__main__':
     now = datetime.datetime.now()
     nowDatetime = now.strftime('%Y-%m-%d %H:%M:%S')
     message = f'수행중 {nowDatetime}'
-    bot.sendMessage(bot_channel, message)
+
+    # print(f'BOT_MSG_LIMIT={BOT_MSG_LIMIT},BOT_MSG_SLEEP={BOT_MSG_SLEEP},bot_msg_count={bot_msg_count}')
+    if (bot_msg_count >= BOT_MSG_LIMIT) and (bot_msg_count % BOT_MSG_LIMIT) == 0:
+        print('bot will not be able to send more than 20 messages per minute to the same group.')
+        print('waiting a minute.')
+        sleep(BOT_MSG_SLEEP)
+    # bot_msg_count += 1
+    # bot.sendMessage(TELEGRAM_CHANNEL, message)
     print(message)
